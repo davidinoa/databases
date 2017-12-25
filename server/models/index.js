5a9c03c97eb6ea8eb4463c07e275a1ce86d474b7
David Inoa <davidinoa@gmail.com>
2017-12-25 14:29:14 -0500

    Implement model for users

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,25 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {},
+    post: function () {}
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      // fetch all users
+      var queryStr = 'select * from users';
+      db.query(queryStr, function(err, result) {
+        callback(results);
+      });
+    },
+    post: function (params, callback) {
+      // create a user
+      var queryStr = 'insert into users(username) values (?)';
+      db.query(queryStr, params, function(err, result) {
+        callback(results);
+      });
+    }
   }
 };
 
